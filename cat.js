c63cbaa9c1ed96fedba1d5cc5ef5f822749f1d8f
Viktor <midyukinv@mail.ru>
2023-06-30 20:26:38 +0300

    add scripts card

---
+++ cat.js
@@ -0,0 +1,40 @@
+export let data = [
+  {
+    title: "Вы котик злодей!",
+    img: "https://www.google.com/search?q=rjnbr+pkj%3Bltq&tbm=isch&ved=2ahUKEwjgkpjRw-b_AhWB_SoKHSaID0IQ2-cCegQIABAA&oq=rjnbr+pkj%3Bltq&gs_lcp=CgNpbWcQDDoHCCMQ6gIQJzoICAAQgAQQsQM6BQgAEIAEOgsIABCKBRAKEAEQQzoJCAAQgAQQChABOgcIABCKBRBDOgQIABAeOgYIABAFEB46BwgAEIAEEAo6BwgAEIAEEAFQrgtYpCdgsTJoAXAAeACAAUWIAfIEkgECMTSYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABA8ABAQ&sclient=img&ei=BnOcZOD3HoH7qwGmkL6QBA&bih=919&biw=1920#imgrc=wbiJWhLnnVPGVM",
+    keywords: "злой злюка злодей зловещий злость злючка злоба",
+  },
+
+  {
+    title: "Вы котик обжора!",
+    img: "https://www.google.com/search?q=rjnbr+pkj%3Bltq&tbm=isch&ved=2ahUKEwjgkpjRw-b_AhWB_SoKHSaID0IQ2-cCegQIABAA&oq=rjnbr+pkj%3Bltq&gs_lcp=CgNpbWcQDDoHCCMQ6gIQJzoICAAQgAQQsQM6BQgAEIAEOgsIABCKBRAKEAEQQzoJCAAQgAQQChABOgcIABCKBRBDOgQIABAeOgYIABAFEB46BwgAEIAEEAo6BwgAEIAEEAFQrgtYpCdgsTJoAXAAeACAAUWIAfIEkgECMTSYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABA8ABAQ&sclient=img&ei=BnOcZOD3HoH7qwGmkL6QBA&bih=919&biw=1920#imgrc=WkQIMFKMWvdbDM",
+    keywords:
+      "голод голодный обжора прожорливый жрать обжираловка покушать кушать есть еда",
+  },
+
+  {
+    title: "Вы котик пуся!",
+    img: "",
+    keywords: "милашка пуся милый малыш забавный милота уиии",
+  },
+
+  {
+    title: "Вы котик зайка!",
+    img: "",
+    keywords: "зайка зайченок зая",
+  },
+
+  {
+    title: "Вы котик соня",
+    img: "",
+    keywords: "ленивый соня сонливый сон спать бездельничать бездельник лень ",
+  },
+
+  {
+    title: "Вы котк дурачек",
+    img: "",
+    keywords: "дурак балбес дибил смешной веселый глупый дурачек дурашка",
+  },
+
+  
+];
---
+++ script.js
@@ -0,0 +1,36 @@
+import { data } from "./cat.js";
+
+const containerCard = document.querySelector(".containerCard");
+const input = document.querySelector("input");
+
+
+
+
+function createCard(obj) {
+  
+  //создаем саму карточу котика
+  let card = document.createElement("div");
+  card.classList.add("catCard");
+
+  let image = document.createElement("img");
+  image.classList.add("image");
+  image.textContent = obj.img;
+
+
+
+  let title = document.createElement("p"); //создаем текст название котика в карточки 
+  title.classList.add("catName"); //обращение к стилям
+  title.textContent = obj.title; // достаем название котика
+
+  card.append(image, title);
+  containerCard.append(card);
+}
+
+
+function reviewCard(arr) {
+  arr.forEach(function (elem) {
+    createCard(elem);
+  });
+} 
+
+reviewCard(data);
